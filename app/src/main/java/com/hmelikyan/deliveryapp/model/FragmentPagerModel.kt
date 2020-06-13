823c83a4c3076065d3349d31d9a67efacb5c50b9
hikemelikyan <hikemelikyan@gmail.com>
2020-06-13 14:13:45 +0400

    locations and map markers are added!

--- app/src/main/java/com/hmelikyan/deliveryapp/model/FragmentPagerModel.kt
+++ app/src/main/java/com/hmelikyan/deliveryapp/model/FragmentPagerModel.kt
@@ -1,5 +1,6 @@
 package com.hmelikyan.deliveryapp.model
 
 import androidx.fragment.app.Fragment
+import com.hmelikyan.deliveryapp.view.root.BaseFragment
 
-data class FragmentPagerModel(val title: String, val fragment: Fragment)
+data class FragmentPagerModel(val title: String, val fragment: BaseFragment)
---
+++ app/src/main/java/com/hmelikyan/deliveryapp/view/activity/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.hmelikyan.deliveryapp.view.activity
+
+import android.os.Bundle
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
+import com.hmelikyan.deliveryapp.R
+import com.hmelikyan.deliveryapp.databinding.ActivityMainBinding
+import com.hmelikyan.deliveryapp.model.FragmentPagerModel
+import com.hmelikyan.deliveryapp.model.OrderDestinationModel
+import com.hmelikyan.deliveryapp.shared.delegates.contentView
+import com.hmelikyan.deliveryapp.shared.delegates.viewModelProvider
+import com.hmelikyan.deliveryapp.view.activity.fragments.MapFragment
+import com.hmelikyan.deliveryapp.view.activity.fragments.StopsFragment
+import com.hmelikyan.deliveryapp.view.adapters.FragmentViewPagerAdapter
+import com.hmelikyan.deliveryapp.view.root.BaseActivity
+import com.hmelikyan.deliveryapp.viewModel.MainViewModel
+import java.io.IOException
+import java.io.InputStream
+import java.nio.charset.Charset
+
+class MainActivity : BaseActivity() {
+    private val mBinding: ActivityMainBinding by contentView(R.layout.activity_main)
+    private val mViewModel: MainViewModel by viewModelProvider()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val destinations = getDataFromJson()
+        destinations[0].isActive = true
+        destinations[4].isFinished = true
+        mViewModel.orderDestinationsLiveData.postValue(destinations)
+
+        val fragmentsList = arrayListOf(
+            FragmentPagerModel(getString(R.string.stops_title), StopsFragment()),
+            FragmentPagerModel(getString(R.string.map_title), MapFragment())
+        )
+
+        val pagerAdapter = FragmentViewPagerAdapter(supportFragmentManager, fragmentsList)
+        mBinding.fragmentPager.adapter = pagerAdapter
+        mBinding.fragmentTabs.setupWithViewPager(mBinding.fragmentPager)
+    }
+
+    private fun getDataFromJson(): ArrayList<OrderDestinationModel> {
+        var json: String? = null
+        try {
+            val inputStream: InputStream = assets.open("locations.json")
+            val size: Int = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            json = String(buffer, Charset.forName("UTF-8"))
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        val jElement = JsonParser().parse(json)
+        val gson = Gson()
+        return gson.fromJson(jElement, object : TypeToken<ArrayList<OrderDestinationModel>>() {}.type)
+    }
+}
